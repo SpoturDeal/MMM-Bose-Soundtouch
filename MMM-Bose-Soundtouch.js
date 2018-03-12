04f662d77d6e5ddd2b11ad881f485dff5991ec0a
Carl <fonts@spoturdeal.com>
2018-03-12 19:54:48 +0100

    Add files via upload

--- MMM-Bose-Soundtouch.js
+++ MMM-Bose-Soundtouch.js
@@ -33,15 +33,15 @@
         var lenAlbum=30;
         var showMusic = false;
         if (music.source == "SPOTIFY"){
-            sTitle = '<i class="fas fa-spotify" style="color:#1ED760;"></i> ' + music.ContentItem.itemName;
+            sTitle = '<i class="fa fa-spotify" style="color:#1ED760;"></i> ' + music.ContentItem.itemName;
             showMusic = true;
             sAlbum=music.album;
         } else if (music.source == "AMAZON"){
-            sTitle = '<i class="fas fa-amazon" style="color:#232F3E;"></i> ' + music.ContentItem.itemName;
+            sTitle = '<i class="fa fa-amazon" style="color:#232F3E;"></i> ' + music.ContentItem.itemName;
             showMusic = true;
             sAlbum=music.album;
         } else if (music.source == "DEEZER" || music.source == "ITUNES" || music.source == "TUNEIN"){
-            sTitle = '<i class="fas fa-music" style="color:#FFFFFF;"></i> ' + music.ContentItem.itemName;
+            sTitle = '<i class="fa fa-music" style="color:#FFFFFF;"></i> ' + music.ContentItem.itemName;
             showMusic = true;
             sAlbum=music.album;
         } else if (music.source == "INTERNET_RADIO"){
@@ -49,7 +49,7 @@
             showMusic = true;
             sAlbum = music.stationName;
         } else if (music.source == "STANDBY"){
-            sTitle ='<i class="fas fa-spotify"></i> stand by';
+            sTitle ='<i class="fa fa-spotify"></i> stand by';
         } else {
             sTitle = "Soundtouch - Your help is needed";
             sAlbum = "Please send me a message with the contents of<br>http:/" + this.config.apiBase + ':8090/now_playing<br>I can add the unknown service.';
@@ -59,10 +59,10 @@
         var text = '<div>';
         text +='<header class="module-header">' + sTitle + '</header>';
         text +='<table>';
-        text +='<tr><td class="small">' + (music.track?this.maxSize(music.track,30):'') + '</td><td class="small"><i class="fas fa-music"></i><td></td><td class="small" rowspan="3">' + (sArt?htmlImage:'') + '</td></tr>'
+        text +='<tr><td class="small">' + (music.track?this.maxSize(music.track,30):'') + '</td><td class="small"><i class="fa fa-music"></i><td></td><td class="small" rowspan="3">' + (sArt?htmlImage:'') + '</td></tr>'
 
-        text += (music.artist?'<tr><td class="small">' + this.maxSize(music.artist,30) + '</td><td class="small"><i class="fas fa-user"></i></td></tr>':'');
-        text += (sAlbum?'<tr><td class="small">' + this.maxSize(sAlbum,lenAlbum) + '</td><td class="small"><i class="fas fa-file-audio-o"></i></td></tr>':'');
+        text += (music.artist?'<tr><td class="small">' + this.maxSize(music.artist,30) + '</td><td class="small"><i class="fa fa-user-o"></i></td></tr>':'');
+        text += (sAlbum?'<tr><td class="small">' + this.maxSize(sAlbum,lenAlbum) + '</td><td class="small"><i class="fa fa-file-audio-o"></i></td></tr>':'');
         text += '</table></div>';
         // if no source found  show nothing
         if (showMusic===false) {
@@ -88,7 +88,7 @@
 	},
 	getStyles: function() {
 		return ['bose.css',
-                'https://use.fontawesome.com/releases/v5.0.8/css/all.css'];
+            'https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css'];
 	},
     maxSize: function(sTr,len){
        if(sTr.length > len) {

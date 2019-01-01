9ea6b646d927284910007a6d9f348e8beca30e75
Carl <fonts@spoturdeal.com>
2019-01-01 18:51:19 +0100

    Quick fix

--- MMM-Bose-Soundtouch.js
+++ MMM-Bose-Soundtouch.js
@@ -36,7 +36,7 @@
             sTitle = '<i class="fa fa-spotify" style="color:#1ED760;"></i> ' + music.ContentItem.itemName;
             showMusic = true;
             sAlbum=music.album;
-        } else if (music.source == "AMAZON"){
+        } else if (music.source == "AMAZON" || music.source == "ALEXA"){
             sTitle = '<i class="fa fa-amazon" style="color:#232F3E;"></i> ' + music.ContentItem.itemName;
             showMusic = true;
             sAlbum=music.album;
